99d576537d794b8c18e096d0024870e27cfc6de0
Chad Redmond <credmond@mercyhurst.edu>
2026-03-30 13:17:17 +0000

    MNIST downloaded

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 from fastapi.staticfiles import StaticFiles
-from fastapi import FastAPI    
+from fastapi import FastAPI
+from torchvision import datasets, transforms
+
+# Download MNIST dataset to ./data and load test set into memory
+transform = transforms.ToTensor()
+test_dataset = datasets.MNIST(root="./data", train=False, download=True, transform=transform)
 
 app = FastAPI()
 
